a9ccfe5cd06d617e93d4c62c3dd98d3aa3e88cde
appconnect-testing <122339450+appconnect-testing@users.noreply.github.com>
2023-01-31 15:33:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,12 @@ import streamlit as st
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Streamlit Template",
-                    page_icon='✅',
-                    initial_sidebar_state='collapsed')
-    st.title('🔨 Streamlit Template')
-    st.markdown("""
-        This app is only a template for a new Streamlit project. <br>
+    st.set_page_config(
+        page_title="fish ai project",
+        page_icon="chart_with_upwards_trend",
+        layout="wide",
+    )
 
-        ---
-        """, unsafe_allow_html=True)
-
-    st.balloons()
     import time  # to simulate a real time data, time loop
     import warnings
     warnings.filterwarnings("ignore")
